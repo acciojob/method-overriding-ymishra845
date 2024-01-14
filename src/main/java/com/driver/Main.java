90d3394b5d1c8d480051036759352eb1facfdf70
Yogesh mishra <139787838+ymishra845@users.noreply.github.com>
2024-01-14 19:06:18 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 B objB = new B();
+		 
+		 System.out.println(objB.meth());
+
+	}
+
 }
