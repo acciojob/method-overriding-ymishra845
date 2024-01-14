645fb18eabc3bb223a37d28fe13e73f8c721e1fe
Yogesh mishra <139787838+ymishra845@users.noreply.github.com>
2024-01-14 19:08:18 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
+public class A {
+	
+	public String meth() {
+		  return "Invoking method from class A";
+	}
+
+}
 public class Main {
 
 	public static void main(String[] args) {
